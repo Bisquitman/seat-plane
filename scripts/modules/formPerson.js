62a2aad57f3b03adb7f76f6829c92d7a675afc5b
Bisquitman <lynx8309@gmail.com>
2022-02-02 12:48:16 +0300

    Day 2

--- scripts/modules/formPerson.js
+++ scripts/modules/formPerson.js
@@ -27,13 +27,34 @@ const createFormPerson = (n) => {
 
   fieldName.append(labelName, inputName);
 
+  const fieldTicket = createElement('div', {
+    className: 'field',
+  });
+  const labelTicket = createElement('label', {
+    className: 'field__label',
+    for: `ticket${n}`,
+    textContent: 'Номер билета (10 цифр)',
+  });
+  const inputTicket = createElement('input', {
+    className: 'field__input',
+    id: `ticket${n}`,
+    name: 'ticket',
+    type: 'text',
+    placeholder: 'Номер билета (10 цифр)',
+    required: true,
+    minLength: 10,
+    maxLength: 10,
+  });
+
+  fieldTicket.append(labelTicket, inputTicket);
+
   const button = createElement('button', {
     className: 'btn-confirm',
     type: 'submit',
     textContent: 'Подтвердить',
   });
 
-  form.append(title, fieldName, button);
+  form.append(title, fieldName, fieldTicket, button);
 
   return form;
 };
